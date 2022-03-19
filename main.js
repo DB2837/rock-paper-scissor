43e5224c367a23d41ea1ad544fd0b75ab6f3929a
davide <davidebellucci975@gmail.com>
2022-03-19 12:22:28 +0100

    add the logic to make player choose

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const possibleChoices = ["rock", "paper", "scissor"];
+let playerChoice;
+let computerChoice;
 
 generateRandomInteger = (max) => Math.floor(Math.random() * max); //generate random integer between 0-2
 computerPlay = () => possibleChoices[generateRandomInteger(3)];
+playerSelection = () => prompt("Make your choice(Rock, Paper, Scissor): ", " "); //ask player to type one of the possible choices
+validatePlayerChoice = (string) => string.toLowerCase(); // makes the choice lower case to match option in possibleCoice array
+
+/* playRound = (computerPlay(), playerSelection()) => ; */
+computerChoice = computerPlay();
+
+playerChoice = validatePlayerChoice(playerSelection());
