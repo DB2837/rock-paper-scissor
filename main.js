edaa42190d645031f33d99c51eaf71b3c9a9cfd0
davide <davidebellucci975@gmail.com>
2022-03-22 11:04:49 +0100

    add feature to change image from default question mark to player choice

--- main.js
+++ main.js
@@ -1,54 +1,86 @@
 const possibleChoices = ["rock", "paper", "scissor"];
+let playerChoice;
+let computerChoice;
+let playerCounter = 0;
+let computerCounter = 0;
 
-const paperButton = document.querySelector("#paper");
-const rockButton = document.querySelector("#rock");
-const scissorButton = document.querySelector("#scissor");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
 
-const myScore = document.querySelector("#my-score");
+const weaponButtons = document.querySelectorAll(".card");
+
+weaponButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    playerChoice = button.id;
+    computerChoice = computerPlay();
+    changePlayerImage(playerChoice);
+    changeComputerImage(computerChoice);
+    playRound();
+  })
+);
+
+const resultDisplay = document.createElement("h3");
+document.body.appendChild(resultDisplay);
+resultDisplay.style.textAlign = "center";
+resultDisplay.style.fontSize = "28px";
+
+const playerChoiceImage = document.getElementById("player-choice");
+const computerChoiceImage = document.getElementById("computer-choice");
 
 generateRandomInteger = (max) => Math.floor(Math.random() * max); //generate random integer between 0-(max - 1)
 
 computerPlay = () =>
   possibleChoices[generateRandomInteger(possibleChoices.length)];
 
-let playerCounter = 0;
-let computerCounter = 0;
-
-function playRound() {
-  const computerChoice = computerPlay();
-
-  if (this.id == computerChoice) {
-    console.log("no one wins, tie");
-  } else if (this.id == "paper" && computerChoice == "rock") {
-    playerCounter++;
-    document.getElementById("player-score").innerHTML = playerCounter;
-    console.log(`You Won! ${this.id} beat ${computerChoice}`);
-  } else if (this.id == "paper" && computerChoice == "scissor") {
-    computerCounter++;
-    document.getElementById("computer-score").innerHTML = computerCounter;
-    console.log(`You Lose! ${computerChoice} beat ${this.id}`);
-  } else if (this.id == "rock" && computerChoice == "scissor") {
-    playerCounter++;
-    document.getElementById("player-score").innerHTML = playerCounter;
-    console.log(`You Won! ${this.id} beat ${computerChoice}`);
-  } else if (this.id == "rock" && computerChoice == "paper") {
-    computerCounter++;
-    document.getElementById("computer-score").innerHTML = computerCounter;
-    console.log(`You Lose! ${computerChoice} beat ${this.id}`);
-  } else if (this.id == "scissor" && computerChoice == "paper") {
-    playerCounter++;
-    document.getElementById("player-score").innerHTML = playerCounter;
-    console.log(`You Won! ${this.id} beat ${computerChoice}`);
-  } else if (this.id == "scissor" && computerChoice == "rock") {
-    computerCounter++;
-    document.getElementById("computer-score").innerHTML = computerCounter;
-    console.log(`You Lose! ${computerChoice} beat ${this.id}`);
-  } else console.log("something went wrong!");
+function changePlayerImage(playerChoice) {
+  if (playerChoice == "paper") {
+    playerChoiceImage.src = "/images/rock-paper-scissors-icon-2.jpg";
+  } else if (playerChoice == "rock") {
+    playerChoiceImage.src = "/images/rock-paper-scissors-icon-3.jpg";
+  } else if (playerChoice == "scissor") {
+    playerChoiceImage.src = "/images/rock-paper-scissors-icon-4.jpg";
+  }
 }
 
-paperButton.addEventListener("click", playRound);
-rockButton.addEventListener("click", playRound);
-scissorButton.addEventListener("click", playRound);
+function changeComputerImage(computerChoice) {
+  if (computerChoice == "paper") {
+    computerChoiceImage.src = "/images/rock-paper-scissors-icon-2.jpg";
+  } else if (computerChoice == "rock") {
+    computerChoiceImage.src = "/images/rock-paper-scissors-icon-3.jpg";
+  } else if (computerChoice == "scissor") {
+    computerChoiceImage.src = "/images/rock-paper-scissors-icon-4.jpg";
+  }
+}
+
+function playRound() {
+  if (playerChoice == computerChoice) {
+    resultDisplay.textContent = `No one wins, it's a draw.`;
+  } else if (playerChoice == "paper" && computerChoice == "rock") {
+    playerCounter++;
+    playerScore.textContent = playerCounter;
+    resultDisplay.textContent = `You Won! ${playerChoice} beat ${computerChoice}`;
+  } else if (playerChoice == "paper" && computerChoice == "scissor") {
+    computerCounter++;
+    computerScore.textContent = computerCounter;
+    resultDisplay.textContent = `You Lose! ${computerChoice} beat ${playerChoice}`;
+  } else if (playerChoice == "rock" && computerChoice == "scissor") {
+    playerCounter++;
+    playerScore.textContent = playerCounter;
+    resultDisplay.textContent = `You Won! ${playerChoice} beat ${computerChoice}`;
+  } else if (playerChoice == "rock" && computerChoice == "paper") {
+    computerCounter++;
+    computerScore.textContent = computerCounter;
+    resultDisplay.textContent = `You Lose! ${computerChoice} beat ${playerChoice}`;
+  } else if (playerChoice == "scissor" && computerChoice == "paper") {
+    playerCounter++;
+    playerScore.textContent = playerCounter;
+    resultDisplay.textContent = `You Won! ${playerChoice} beat ${computerChoice}`;
+  } else if (playerChoice == "scissor" && computerChoice == "rock") {
+    computerCounter++;
+    computerScore.textContent = computerCounter;
+    resultDisplay.textContent = `You Lose! ${computerChoice} beat ${playerChoice}`;
+  } else console.log("something went wrong!");
+}
 
 /* function game() {
   for (let i = 0; i < 5; i++) {
