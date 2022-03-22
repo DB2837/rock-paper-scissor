19b509cad3cd144e200a2710573c96a29acdd647
davide <davidebellucci975@gmail.com>
2022-03-22 14:30:14 +0100

    add feature for displaying whit colors the winner/loser of the round

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ weaponButtons.forEach((button) =>
     changePlayerImage(playerChoice);
     changeComputerImage(computerChoice);
     playRound();
+
+    if (playerCounter >= 5 || computerCounter >= 5) {
+      removeColor();
+      resetGame();
+    }
   })
 );
 
@@ -26,6 +31,9 @@ resultDisplay.style.fontSize = "28px";
 
 const playerChoiceImage = document.getElementById("player-choice");
 const computerChoiceImage = document.getElementById("computer-choice");
+
+const playerCard = document.querySelector(".player-card");
+const computerCard = document.querySelector(".computer-card");
 
 generateRandomInteger = (max) => Math.floor(Math.random() * max); //generate random integer between 0-(max - 1)
 
@@ -52,34 +60,76 @@ function changeComputerImage(computerChoice) {
   }
 }
 
+function handleColorPlayerWin() {
+  playerCard.classList.add("card-winner");
+  playerCard.classList.remove("card-loser");
+  computerCard.classList.remove("card-winner");
+  computerCard.classList.add("card-loser");
+}
+
+function handleColorPlayerLose() {
+  playerCard.classList.remove("card-winner");
+  playerCard.classList.add("card-loser");
+  computerCard.classList.remove("card-loser");
+  computerCard.classList.add("card-winner");
+}
+
+function removeColor() {
+  playerCard.classList.remove("card-winner");
+  playerCard.classList.remove("card-loser");
+  computerCard.classList.remove("card-winner");
+  computerCard.classList.remove("card-loser");
+}
+
 function playRound() {
   if (playerChoice == computerChoice) {
+    removeColor();
     resultDisplay.textContent = `No one wins, it's a draw.`;
   } else if (playerChoice == "paper" && computerChoice == "rock") {
     playerCounter++;
+    handleColorPlayerWin();
     playerScore.textContent = playerCounter;
     resultDisplay.textContent = `You Won! ${playerChoice} beat ${computerChoice}`;
   } else if (playerChoice == "paper" && computerChoice == "scissor") {
     computerCounter++;
+    handleColorPlayerLose();
     computerScore.textContent = computerCounter;
     resultDisplay.textContent = `You Lose! ${computerChoice} beat ${playerChoice}`;
   } else if (playerChoice == "rock" && computerChoice == "scissor") {
     playerCounter++;
+    handleColorPlayerWin();
     playerScore.textContent = playerCounter;
     resultDisplay.textContent = `You Won! ${playerChoice} beat ${computerChoice}`;
   } else if (playerChoice == "rock" && computerChoice == "paper") {
     computerCounter++;
+    handleColorPlayerLose();
     computerScore.textContent = computerCounter;
     resultDisplay.textContent = `You Lose! ${computerChoice} beat ${playerChoice}`;
   } else if (playerChoice == "scissor" && computerChoice == "paper") {
     playerCounter++;
+    handleColorPlayerWin();
     playerScore.textContent = playerCounter;
     resultDisplay.textContent = `You Won! ${playerChoice} beat ${computerChoice}`;
   } else if (playerChoice == "scissor" && computerChoice == "rock") {
     computerCounter++;
+    handleColorPlayerLose();
     computerScore.textContent = computerCounter;
     resultDisplay.textContent = `You Lose! ${computerChoice} beat ${playerChoice}`;
   } else console.log("something went wrong!");
+}
+
+function resetGame() {
+  if (playerCounter > computerCounter) {
+    resultDisplay.textContent = `You Won the Game \n ${playerCounter} - ${computerCounter} !`;
+  } else {
+    resultDisplay.textContent = `You Lost the Game \n ${computerCounter} - ${playerCounter} !`;
+  }
+
+  playerCounter = 0;
+  computerCounter = 0;
+
+  playerScore.textContent = playerCounter;
+  computerScore.textContent = computerCounter;
 }
 
 /* function game() {
